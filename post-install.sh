87f26e398fc108d66798dd67fe319b8df308569e
wangxy918 <wangxy918@foxmail.com>
2021-02-21 15:29:00 +0000

    add openclash

---
+++ post-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+
+OPENWRT_PATH=`pwd`
+echo "Post-Install Script [Start]"
+
+echo "Post-Install Script [End]"
